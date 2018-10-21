aab247c8aedc03cacb74a9f3cc557b7a1a135a85
sirishadba <sirisha.dba9@gmail.com>
2018-10-21 11:43:05 +0530

    first framework commit

---
+++ MyMavenProject/src/test/java/com/testing/GoogleTest.java
@@ -0,0 +1,10 @@
+package com.testing;
+
+import org.junit.Test;
+
+public class GoogleTest {
+	@Test
+    public void login() {
+	System.out.println("hello");
+}
+}
---
+++ MyMavenProject/src/test/java/com/testing/YahooTest.java
@@ -0,0 +1,19 @@
+package com.testing;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class YahooTest{
+	
+	@Test
+	public void testGoogle(){
+		System.out.println("hello");
+	}
+  
+}
